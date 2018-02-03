691163f2869847089d5c2ecb4935320512632b5b
medrz <36087247+medrz@users.noreply.github.com>
2018-02-03 12:43:27 +0100

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,26 @@
+<?php
+    $con = mysqli_connect("databases-auth.000webhost.com", "id4524168_med", "medrz2011", "id4524168_med2010");
+    
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    $statement = mysqli_prepare($con, "SELECT * FROM user WHERE username = ? AND password = ?");
+    mysqli_stmt_bind_param($statement, "ss", $username, $password);
+    mysqli_stmt_execute($statement);
+    
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $userID, $name, $age, $username, $password);
+    
+    $response = array();
+    $response["success"] = false;  
+    
+    while(mysqli_stmt_fetch($statement)){
+        $response["success"] = true;  
+        $response["name"] = $name;
+        $response["age"] = $age;
+        $response["username"] = $username;
+        $response["password"] = $password;
+    }
+    
+    echo json_encode($response);
+?>
