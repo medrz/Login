8bd55cff0f4d4c811e3e1c18b230eb45afc48d2a
medrz <36087247+medrz@users.noreply.github.com>
2018-02-03 12:46:58 +0100

    Create Register.php

---
+++ Register.php
@@ -0,0 +1,16 @@
+# <?php
+    $con = mysqli_connect("databases-auth.000webhost.com", "id4524168_med", "medrz2011", "id4524168_med2010");
+    
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, age, password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $name, $username, $age, $password);
+    mysqli_stmt_execute($statement);
+    
+    $response = array();
+    $response["success"] = true;  
+    
+    echo json_encode($response);
+?>
